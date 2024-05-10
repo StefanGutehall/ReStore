6137592ab6cc0150583c870757b493ed6a3faaf9
StefanGutehall <stefan@gutehall.com>
2024-05-10 16:51:35 +0800

    End of section 3

---
+++ client/src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './app/layout/App.tsx';
+import './app/layout/styles.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
+/* const root = ReactDOMN.createRoot(
+  document.getElementById('root') as HTMLElement
+); */
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
